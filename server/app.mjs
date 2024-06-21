6926e3e6187121a511218ed393e2dd2b3648fa1b
James <apisitamornktn@gmail.com>
2024-06-21 14:58:41 +0700

    feat:add server at port 4000

---
+++ server/app.mjs
@@ -0,0 +1,19 @@
+import express from "express";
+
+const app = express();
+const port = 4000;
+
+const johnProfile = {
+  data: {
+    name: "john",
+    age: 20,
+  },
+};
+
+app.get("/profiles", (req, res) => {
+  return res.status(200).json(johnProfile);
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
